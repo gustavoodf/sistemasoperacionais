277bae755eca3a2355c8ef7b2c9d6a0d386762a5
gustavoodf <gustavoferreira_trabalho@hotmail.com>
2025-08-29 21:26:56 -0300

    Add files via upload

---
+++ sistemasoi.py
@@ -0,0 +1,17 @@
+# importação de biblioteca
+import threading
+import time
+import math
+#Estrutura da Thread
+def estruturaThread(nome,inicio,fim):
+    for i in range(inicio, fim +1):
+        print(f"{nome}-> {i}")
+        time.sleep(1000)
+
+thread1 = threading.Thread(target=estruturaThread, args=("Thread1", 1, 10))
+thread2 = threading.Thread(target=estruturaThread, args=("Thread1", 11, 20))
+
+thread1.start()
+thread2.start()     
+
+    
